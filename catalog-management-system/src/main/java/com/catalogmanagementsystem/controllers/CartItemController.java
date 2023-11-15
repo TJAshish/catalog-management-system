1302035d65b9664e4442903b138857a6abd9a9f5
TJAshish <140999950+TJAshish@users.noreply.github.com>
2023-11-15 08:06:43 +0530

    CartItemController.java

--- catalog-management-system/src/main/java/com/catalogmanagementsystem/controllers/CartItemController.java
+++ catalog-management-system/src/main/java/com/catalogmanagementsystem/controllers/CartItemController.java
@@ -1,54 +1,48 @@
 package com.catalogmanagementsystem.controllers;
 
 import com.catalogmanagementsystem.entities.CartItem;
-import com.catalogmanagementsystem.entities.Product;
-import com.catalogmanagementsystem.entities.User;
 import com.catalogmanagementsystem.services.CartItemService;
 
 import jakarta.validation.Valid;
 
-import com.catalogmanagementsystem.exceptions.CartItemNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/cartItems")
+@RequestMapping("/api/cartitems")
 public class CartItemController {
-    @Autowired
+
+	@Autowired
     private CartItemService cartItemService;
 
-    @GetMapping("/{id}")
-    public ResponseEntity<CartItem> getCartItem(@PathVariable Long id) {
-        try {
-            CartItem cartItem = cartItemService.findById(id);
+    @GetMapping("/{cartItemId}")
+    public ResponseEntity<CartItem> getCartItemById(@PathVariable Long cartItemId) {
+        CartItem cartItem = cartItemService.findCartItemById(cartItemId);
+        if (cartItem != null) {
             return ResponseEntity.ok(cartItem);
-        } catch (CartItemNotFoundException ex) {
+        } else {
             return ResponseEntity.notFound().build();
         }
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteCartItem(@PathVariable Long id) {
-        try {
-            cartItemService.deleteById(id);
-            return ResponseEntity.noContent().build();
-        } catch (CartItemNotFoundException ex) {
-            return ResponseEntity.notFound().build();
-        }
+    @GetMapping
+    public ResponseEntity<List<CartItem>> getAllCartItems() {
+        List<CartItem> cartItems = cartItemService.findAllCartItems();
+        return ResponseEntity.ok(cartItems);
     }
 
-    @PostMapping("/addProductToCart")
-    public ResponseEntity<CartItem> addProductToCart(  @Valid
-            @RequestParam Long userId,
-            @RequestParam Long productId,
-            @RequestParam int quantity
-    ) {
-        
-        User user = new User(); 
-        Product product = new Product(); 
+    @PostMapping
+    public ResponseEntity<CartItem> createCartItem(@RequestBody CartItem cartItem) {
+        CartItem savedCartItem = cartItemService.saveCartItem(cartItem);
+        return ResponseEntity.ok(savedCartItem);
+    }
 
-        cartItemService.addProductToCart(user, product, quantity);
-        return ResponseEntity.ok().build();
+    @DeleteMapping("/{cartItemId}")
+    public ResponseEntity<Void> deleteCartItem(@PathVariable Long cartItemId) {
+        cartItemService.deleteCartItem(cartItemId);
+        return ResponseEntity.noContent().build();
     }
 }
