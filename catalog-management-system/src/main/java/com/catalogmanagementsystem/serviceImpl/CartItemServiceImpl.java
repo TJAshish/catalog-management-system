777256ef0361e5c18ad5795c65cfd3dae2f9e251
TJAshish <140999950+TJAshish@users.noreply.github.com>
2023-11-15 08:05:08 +0530

    CartItemServiceImpl.java

--- catalog-management-system/src/main/java/com/catalogmanagementsystem/serviceImpl/CartItemServiceImpl.java
+++ catalog-management-system/src/main/java/com/catalogmanagementsystem/serviceImpl/CartItemServiceImpl.java
@@ -1,57 +1,36 @@
 package com.catalogmanagementsystem.serviceImpl;
 
-import java.util.Optional;
-
+import com.catalogmanagementsystem.entities.CartItem;
+import com.catalogmanagementsystem.repository.CartItemRepository;
+import com.catalogmanagementsystem.services.CartItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.catalogmanagementsystem.entities.CartItem;
-import com.catalogmanagementsystem.entities.Product;
-import com.catalogmanagementsystem.entities.User;
-import com.catalogmanagementsystem.exceptions.CartItemNotFoundException;
-import com.catalogmanagementsystem.repository.CartItemRepository;
-import com.catalogmanagementsystem.services.CartItemService;
+import java.util.List;
 
 @Service
 public class CartItemServiceImpl implements CartItemService {
-	
+
 	@Autowired
     private CartItemRepository cartItemRepository;
 
-
     @Override
-    public CartItem findById(Long id) {
-        Optional<CartItem> cartItem = cartItemRepository.findById(id);
-        if (cartItem.isEmpty()) {
-            throw new CartItemNotFoundException("Cart item with ID " + id + " not found");
-        }
-        return cartItem.get();
-    }
-
-
-    @Override
-    public void deleteById(Long id) {
-        if (!cartItemRepository.existsById(id)) {
-            throw new CartItemNotFoundException("Cart item with ID " + id + " not found");
-        }
-        cartItemRepository.deleteById(id);
+    public CartItem findCartItemById(Long cartItemId) {
+        return cartItemRepository.findById(cartItemId).orElse(null);
     }
 
     @Override
-    public void addProductToCart(User user, Product product, int quantity) {
-        
-        CartItem existingCartItem = cartItemRepository.findByUserAndProduct(user, product);
-
-        // If the cart item exists, update the quantity
-        if (existingCartItem != null) {
-            existingCartItem.setQuantity(existingCartItem.getQuantity() + quantity);
-            cartItemRepository.save(existingCartItem);
-        } else {
-            // If the cart item does not exist, create a new one
-            CartItem newCartItem = new CartItem();
-            cartItemRepository.save(newCartItem);
-        }
+    public List<CartItem> findAllCartItems() {
+        return cartItemRepository.findAll();
     }
 
+    @Override
+    public CartItem saveCartItem(CartItem cartItem) {
+        return cartItemRepository.save(cartItem);
+    }
+
+    @Override
+    public void deleteCartItem(Long cartItemId) {
+        cartItemRepository.deleteById(cartItemId);
+    }
 }
-
